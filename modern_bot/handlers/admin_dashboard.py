74b9377c9ede16985ba8d5f3aa9de862bd4b979c
Oleg Zhukov <oleg@Olegs-MacBook-Pro.local>
2025-11-28 16:01:07 +0300

    Bump: Web App version to 2.7

--- modern_bot/handlers/admin_dashboard.py
+++ modern_bot/handlers/admin_dashboard.py
@@ -16,7 +16,7 @@ async def admin_dashboard_handler(update: Update, context: CallbackContext) -> N
         return
     
     # WebApp URL from config
-    web_app_url = "https://olegfire07.github.io/botbot/web_app/?v=2.6"
+    web_app_url = "https://olegfire07.github.io/botbot/web_app/?v=2.7"
     
     keyboard = [
         [
